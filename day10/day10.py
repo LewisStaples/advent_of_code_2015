81c1ba4a226f79bb0506c639f4c44bf5e21ffdd5
Lewis Staples <lewis.staples@gmail.com>
2022-07-25 16:32:33 -0400

    Day 10 Improved efficiency by storing as a list of integers instead of a string

--- day10/day10.py
+++ day10/day10.py
@@ -3,24 +3,26 @@
 
 import time
 
-def update_string(curr_str):
-    new_str = ''
+def update_list(curr_str_list):
+    new_str_list = []
     next_sequence = {
         'num_repeats': 1,
         'digit_ch': curr_str[0]
     }
-    for i in range(1, len(curr_str)):
-        if curr_str[i] == next_sequence['digit_ch']:
+    for i in range(1, len(curr_str_list)):
+        if curr_str_list[i] == next_sequence['digit_ch']:
             # Add to the prior sequence
             next_sequence['num_repeats'] += 1
         else:
-            new_str += str(next_sequence['num_repeats']) + next_sequence['digit_ch']
+            new_str_list.append(next_sequence['num_repeats'])
+            new_str_list.append(next_sequence['digit_ch'])
             next_sequence = {
                 'num_repeats': 1,
-                'digit_ch': curr_str[i]
+                'digit_ch': curr_str_list[i]
             }
-    new_str += str(next_sequence['num_repeats']) + next_sequence['digit_ch']
-    return new_str
+    new_str_list.append(next_sequence['num_repeats'])
+    new_str_list.append(next_sequence['digit_ch'])
+    return new_str_list
 
 # Reading input from the input file
 input_filename='input_sample0.txt'
@@ -30,18 +32,21 @@ with open(input_filename) as f:
     # Pull in each line from the input file
     for in_string in f:
         in_string = in_string.rstrip()
-        # print(in_string)
         rep_num, curr_str = in_string.split(',')
         print(f'string: {curr_str} will be processed {rep_num} time(s)')
         rep_num = int(rep_num)
 
         start_time = time.time()
+        curr_str_list = list(curr_str)
         for i in range(rep_num):
-            curr_str = update_string(curr_str)
+            curr_str_list = update_list(curr_str_list)
         end_time = time.time()
-        if len(curr_str) < 60:
-            print(f'     result: {curr_str}')
+        if len(curr_str_list) < 60:
+            print('     result: ', end='')
+            for ch in curr_str_list:
+                print(ch, end='')
+            print()
         else:
-            print(f'The resulting string has a length of {len(curr_str)}')
+            print(f'The resulting string has a length of {len(curr_str_list)}')
         print(f'This took {end_time - start_time} seconds to run')
 print()
