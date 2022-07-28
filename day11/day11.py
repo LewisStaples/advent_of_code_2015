26401351be205aef13ac6b1305ba11f91a54d6e7
Lewis Staples <lewis.staples@gmail.com>
2022-07-27 23:58:21 -0400

    Day 11 Progress made toward validating password, but not yet complete.

--- day11/day11.py
+++ day11/day11.py
@@ -4,7 +4,7 @@
 def increment_password(santas_password):
     str_len = len(santas_password)
     for i in range(str_len - 1, -1, -1):
-        print(f'index: {i}, char: {santas_password[i]}')
+        # print(f'index: {i}, char: {santas_password[i]}')
         if santas_password[i] == 'z':
             santas_password[i] = 'a'
             continue
@@ -12,18 +12,51 @@ def increment_password(santas_password):
         break
     return santas_password
 
+def requirement_one_fails(santas_password):
+    for i in range(0, len(santas_password)-2):
+        if ord(santas_password[i]) + 1 == ord(santas_password[i + 1]):
+            if ord(santas_password[i + 1]) + 1 == ord(santas_password[i + 2]):
+                # Requirement 1 has been met
+                return False
+    return True
+
+def requirement_two_fails(santas_password):
+    for ch in 'iol':
+        if ch in santas_password:
+            return True
+    return False
+
+
+def requirement_three_fails(santas_password):
+    i_match = None
+    for i in range(0, len(santas_password)-1):
+        if ord(santas_password[i]) + 1 == ord(santas_password[i + 1]):
+            if i_match is None:
+                i_match = i
+            else:
+                if i_match > i+1:
+                    return False
+    return True
+
+def is_valid(santas_password):
+    if requirement_one_fails(santas_password):
+        return False
+    if requirement_two_fails(santas_password):
+        return False
+    # if requirement_three_fails(santas_password):
+        # return False
+    return True
+
 # Reading input from the input file
-input_filename='input_scenario0.txt'
+input_filename='input_sample3.txt'
 print(f'\nUsing input file: {input_filename}')
 with open(input_filename) as f:
     santas_password = list(f.readline().rstrip())
 print(f"This file's contents are:  {''.join(santas_password)}\n")
 
 while True:
-    # increment password
     santas_password = increment_password(santas_password)
-    # check if valid
-    if True:
+    if is_valid(santas_password):
         break
 
 print(f'The resulting password is: {"".join(santas_password)}')
