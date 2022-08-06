2679e2ae1b20a89811c6b9fb056ad8cef6ef2c13
Lewis Staples <lewis.staples@gmail.com>
2022-08-05 23:50:24 -0400

    Day 15 Starting to implement recursion to select ingredient amounts

--- day15/day15.py
+++ day15/day15.py
@@ -3,8 +3,6 @@
 
 from collections import OrderedDict
 
-
-
 class IngredientData:
     def __init__(self):
         self._ingredients = {}
@@ -19,6 +17,8 @@ class IngredientData:
         texture = int(input_list[8][:-1])
         calories = int(input_list[10])
 
+        # Index is ingred. name, 
+        # value is ingred. characteristics per unit
         self._ingredients[ingredient_name] = {
             'capacity': capacity,
             'durability': durability,
@@ -27,7 +27,15 @@ class IngredientData:
             'calories': calories
         }
 
+    def get_total_score(self, ingredientAmounts):
+        ret_val = 43
+
         dummy = 123
+
+
+        return ret_val
+
+
 
     def createIngAmts(self):
         ret_val = OrderedDict()
@@ -36,12 +44,27 @@ class IngredientData:
         return ret_val
 
 
+    def choose_next_ingred_amount(self, ingredientAmounts, ingred_amt_loop_upper_value, ingredientData):
+        i_ing_next = list(ingredientAmounts.values()).index(None)
+        if i_ing_next == len(ingredientAmounts) - 1:
+            # NEED TO FIX ...
+            # ingredientAmounts[-1] = ingred_amt_loop_upper_value - 1
+            return self.get_total_score(ingredientAmounts)
+
+        ret_val = float('-inf')
+        for ingred_amount in range(0, ingred_amt_loop_upper_value):
+            # NEED TO FIX ....
+            # ingredientAmounts[self._ingredients[i_ing_next]] = ingred_amount
+            ingred_amt_loop_upper_value -= ingred_amount
+            # FIXING ABOVE SHOULD FIX THIS .....
+            # ret_val = max(ret_val, self.choose_next_ingred_amount(ingredientAmounts, ingred_amt_loop_upper_value, ingredientData))
+        return ret_val
 
 
 ingredientData = IngredientData()
 
 # Reading input from the input file
-input_filename='input.txt'
+input_filename='input_sample0.txt'
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
     # Pull in each line from the input file
@@ -55,6 +78,8 @@ TOTAL_AMOUNT = 100
 ingred_amt_loop_upper_value = TOTAL_AMOUNT + 1
 max_score = float('-inf')
 
+# next try recursion to choose ingredient amounts 
+ingredientData.choose_next_ingred_amount(ingredientAmounts, ingred_amt_loop_upper_value, ingredientData)
 
-
+dummy = 123
 
