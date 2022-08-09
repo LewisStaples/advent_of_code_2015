9b0ef1809fa2cd2871f2ac6cf525d125dcddad10
Lewis Staples <lewis.staples@gmail.com>
2022-08-09 17:20:16 -0400

    Day 17 part B working

--- day17/day17.py
+++ day17/day17.py
@@ -4,9 +4,11 @@
 import itertools
 
 container_list = list()
-total_combinations = 0
+total_combinations = 0 # part A answer
+container_dict = dict() # part B answer
+
 # Reading input from the input file
-input_filename='input.txt'
+input_filename='input_sample0.txt'
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
     # Pull in each line from the input file
@@ -16,9 +18,13 @@ with open(input_filename) as f:
 
 for r in range(1, len(container_list)):
     for combo in itertools.combinations(container_list, r):
-        if sum(combo) == 150:
+        if sum(combo) == 25:
             total_combinations += 1 
-    dummy = 123
+            if len(combo) in container_dict:
+                container_dict[len(combo)] += 1
+            else:
+                container_dict[len(combo)] = 1
 
-print(f'The total combinations (part A answer) is: {total_combinations}\n')
+print(f'The total combinations (part A answer) is: {total_combinations}')
+print(f'The count of choices for the minimum number of containers (part B answer) is: {container_dict[min(container_dict.keys())]}\n')
 
