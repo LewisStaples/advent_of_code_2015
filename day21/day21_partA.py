84726b3a89d4df616313c44f00839502495b404b
Lewis Staples <lewis.staples@gmail.com>
2022-08-19 23:40:41 -0400

    Day 21 Making progress towards using brute force to consider all possible purchases of weapons, armor, and rings.

--- day21/day21_partA.py
+++ day21/day21_partA.py
@@ -1,7 +1,20 @@
 # adventOfCode 2015 day 21
 # https://adventofcode.com/2015/day/21
 
+
+# Plan for how to solve part A:
+# Brute force: consider all possible combinations of purchases of weapons, armor, rings
+#   calculate the cost, damage, and armor for each combination
+#   keep the lowest price option in dict brute_forced_options
+#
+# Run every combination of purchases through Game.run_game()
+#   eliminate all items in dict brute_forced_options where player loses
+#   
+# Then sort the values in brute_forced_options ... the smallest cost is the answer to part A.
+
+
 from dataclasses import dataclass
+from itertools import product, combinations
 
 class LogicError(Exception):
     pass
@@ -38,13 +51,18 @@ class Game:
         print()
 
     # This returns the hit points of the defending player
+    # This returns None if there is no winner, or the winning player's name
     def play_round(self):
         # play the round
         index_attacker = self._round_number % 2
         index_defender = (index_attacker + 1) % 2
         self._players[index_defender]._hit_points -= max(1, self._players[index_attacker]._damage - self._players[index_defender]._armor)
         self._round_number += 1
-        return self._players[index_defender]._hit_points
+        # return self._players[index_defender]._hit_points
+        if self._players[index_defender]._hit_points <= 0:
+            return self._players[index_attacker]._name
+        # implicit else
+        return None
 
     def run_game(self, show_all_rounds = True):
         if len(self._players) != 2:
@@ -54,14 +72,72 @@ class Game:
             if show_all_rounds:
                 # Displaying status before the round is played
                 self.display_status()
-            if self.play_round() == 0:
+            if self.play_round() is not None:
                 # Stop playing
                 break
         # Displaying final status
         self.display_status()
         print()
-        
-the_game = Game()
-the_game.add_player( Player(8, 5, 5, 'The player') )
-the_game.add_player( Player(12, 7, 2, 'The boss') )
-the_game.run_game()
+
+# # Code created to test the above code ...    
+# the_game = Game()
+# the_game.add_player( Player(8, 5, 5, 'The player') )
+# the_game.add_player( Player(12, 7, 2, 'The boss') )
+# the_game.run_game()
+
+# Use brute force, considering all possible purchases, to determine all possible total combinations of price, damage, and armor.  If there are any repeats of damage and armor, then only consider the lowest priced version for that combination
+
+price_list = dict()
+# Reading input from the input file
+input_filename='shop_price_list.txt'
+print(f'\nUsing input file: {input_filename}\n')
+with open(input_filename) as f:
+    # Pull in each line from the input file
+    item_category = None
+    for in_string in f:
+        in_string = in_string.rstrip()
+        if len(in_string) == 0:
+            continue
+            item_category = None
+        if in_string[0].islower():
+            item_category = in_string
+            price_list[item_category] = set()
+        else:
+            [item_name, cost, damage, armor] = in_string.split()
+            price_list[item_category].add( (int(cost), int(damage), int(armor)) )
+print(price_list)
+
+# You must buy exactly one weapon
+# You can buy either 0 or 1 pieces of armor
+# You can buy 0, 1, or 2 rings
+purchase_amount_options_dict = {
+    'weapons': [1],
+    'armor': [0,1],
+    'rings': [0,1,2]
+}
+purchase_type = ['weapons', 'armor', 'rings']
+
+purchase_amount_options_iter = product(
+    purchase_amount_options_dict['weapons'],
+    purchase_amount_options_dict['armor'],
+    purchase_amount_options_dict['rings']
+)
+
+# purchase_amount_options_iter = product(x for x in purchase_type)
+print()
+for option in purchase_amount_options_iter:
+    print(option)
+    for i, item_type_quantity in enumerate(option):
+        print(f'{item_type_quantity} of {purchase_type[i]}(s)')
+print()
+
+
+# key = (damage, armor) combination
+# value = lowest seen price
+brute_forced_options = dict()
+
+
+
+
+
+
