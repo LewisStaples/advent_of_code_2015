9020d847defdbf2feed8a2a76298222ed21657be
Lewis Staples <lewis.staples@gmail.com>
2022-08-23 21:33:33 -0400

    Day 21 Code cleanup ... hopefully it is easier to follow!

--- day21/day21_partB.py
+++ day21/day21_partB.py
@@ -2,7 +2,7 @@
 # https://adventofcode.com/2015/day/21
 
 
-# Plan for how to solve part B:
+# Plan for how to solve the problem:
 # Brute force: consider all possible combinations of purchases of weapons, armor, rings
 #   calculate the cost, damage, and armor for each combination
 #   keep the lowest price option in dict brute_forced_options
@@ -52,40 +52,36 @@ class Game:
             print(f' |{player._hit_points:9}', end='')
         print()
 
-    # This returns the hit points of the defending player
     # This returns None if there is no winner, or the winning player's name
     def play_round(self):
-        # play the round
         index_attacker = self._round_number % 2
         index_defender = (index_attacker + 1) % 2
         self._players[index_defender]._hit_points -= max(1, self._players[index_attacker]._damage - self._players[index_defender]._armor)
         self._round_number += 1
-        # return self._players[index_defender]._hit_points
+        # If the attacker has won, return the attacker's name (who is the winner)
         if self._players[index_defender]._hit_points <= 0:
             return self._players[index_attacker]._name
-        # implicit else
+        # If there isn't a winner yet, return None
         return None
 
-    def run_game(self, show_all_rounds = True):
+    def run_game(self, show_rounds = True):
         if len(self._players) != 2:
             raise LogicError('You can only play with exactly two players')
-        # while self._round_number < 8:
         while True:
-            # if show_all_rounds:
-            #     # Displaying status before the round is played
-            #     self.display_status()
-            # if self.play_round() is not None:
-            #     # Stop playing
-            #     break
+            if show_rounds:
+                # Displaying status before the round is played
+                self.display_status()
             the_winner = self.play_round()
             if the_winner is not None:
                 break
         # Displaying final status
-        # self.display_status()
-        # print()
+        if show_rounds:
+            self.display_status()
+            print()
         return the_winner
 
-# Code created to test the above code ...    
+# Code created to test the above code
+# by reproducing the given sample data 
 the_game = Game()
 the_game.add_player( Player(8, 5, 5, 'The player') )
 the_game.add_player( Player(12, 7, 2, 'The boss') )
@@ -106,7 +102,7 @@ class PurchaseCharacteristics:
 brute_forced_options = dict()
 
 price_list = dict()
-# Reading input from the input file
+# Reading input from the price list file
 input_filename='shop_price_list.txt'
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
@@ -115,8 +111,8 @@ with open(input_filename) as f:
     for in_string in f:
         in_string = in_string.rstrip()
         if len(in_string) == 0:
-            continue
             item_category = None
+            continue
         if in_string[0].islower():
             item_category = in_string
             price_list[item_category] = set()
@@ -125,9 +121,9 @@ with open(input_filename) as f:
             price_list[item_category].add( (int(cost), int(damage), int(armor)) )
 print(price_list)
 
-# You must buy exactly one weapon
-# You can buy either 0 or 1 pieces of armor
-# You can buy 0, 1, or 2 rings
+# You must use exactly one weapon
+# You can use either 0 or 1 pieces of armor
+# You can use 0, 1, or 2 rings
 purchase_amount_options_dict = {
     'weapons': [1],
     'armor': [0,1],
@@ -141,9 +137,7 @@ purchase_amount_options_iter = product(
     purchase_amount_options_dict['rings']
 )
 
-print()
-
-# Reading input from the input file
+# Reading input from the puzzle input file
 input_filename='input.txt'
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
@@ -159,49 +153,43 @@ with open(input_filename) as f:
         elif var_name == 'Armor':
             boss_armor = value_int
 
-dummy = 123
-
-for option in purchase_amount_options_iter:
-    # print(f'Option: {option}')
-    options = []
-    for i, item_type_quantity in enumerate(option):
-        # print(f'{item_type_quantity} of {purchase_type[i]}')
+# purchase_quantities_per_category is .. quantities of items to purchase per category
+for purchase_quantities_per_category in purchase_amount_options_iter:
+    list_of_purchase_options = []
+    for i, item_type_quantity in enumerate(purchase_quantities_per_category):
         if i == 0:
-            for items in combinations(price_list[purchase_type[i]], item_type_quantity):
-                for item in items:
-                    options.append([np.asarray(item)])
+            # solo__cost_dmg_armr .. cost, damage, armor values for a single item
+            # grp__cost_dmg_armr .. cost, damage, armor values for a single type of item (it could be a pair of rings)
+            for grp__cost_dmg_armr in combinations(price_list[purchase_type[i]], item_type_quantity):
+                for solo__cost_dmg_armr in grp__cost_dmg_armr:
+                    list_of_purchase_options.append([np.asarray(solo__cost_dmg_armr)])
         else:
         #   go through each item in options and copy it so there are enough to cover all possibilities for the next item type, and append that to the list
-            new_options = []
-            for option_i in range(len(options)):
-                for items in combinations(price_list[purchase_type[i]], item_type_quantity):
-                    new_options.append(copy.deepcopy(options[option_i]))
-                    for item in items:
-                        new_options[-1].append(np.asarray(item))
-            if len(new_options) > 0:
-                options = new_options
+            new_list_of_purchase_options = []
+            for option_i in range(len(list_of_purchase_options)):
+                for grp__cost_dmg_armr in combinations(price_list[purchase_type[i]], item_type_quantity):
+                    new_list_of_purchase_options.append(copy.deepcopy(list_of_purchase_options[option_i]))
+                    for solo__cost_dmg_armr in grp__cost_dmg_armr:
+                        new_list_of_purchase_options[-1].append(np.asarray(solo__cost_dmg_armr))
+            if len(new_list_of_purchase_options) > 0:
+                list_of_purchase_options = new_list_of_purchase_options
 
-    # print()
-    for option in options:
-        # print(f'Option {option}')
-        option_combined = np.array([0,0,0])
-        for nparray in option:
-            option_combined += nparray
+    for single_purchase_option in list_of_purchase_options:
+        single_purchase_option__totaled_cost_dmg_armr = np.array([0,0,0])
+        # Total up the values of cost, damage, and armor for a given group of purchased items
+        for nparray in single_purchase_option:
+            single_purchase_option__totaled_cost_dmg_armr += nparray
         
-        # print(f'Combined: {option_combined}')
-
         the_game = Game()
-        the_game.add_player( Player(100, option_combined[1], option_combined[2], 'The player') )
+        the_game.add_player( Player(100, single_purchase_option__totaled_cost_dmg_armr[1], single_purchase_option__totaled_cost_dmg_armr[2], 'The player') )
         the_game.add_player( Player(boss_hit_points, boss_damage, boss_armor, 'The boss') )
-        # print(f'The winner is ... {the_game.run_game()}')
-        if the_game.run_game() == 'The boss':
+        if the_game.run_game(False) == 'The boss':
             dummy = 123
-            # brute_forced_options[(option_combined[1], option_combined[2])] = option_combined[0]
-            player_variables = (option_combined[1], option_combined[2])
+            player_variables = (single_purchase_option__totaled_cost_dmg_armr[1], single_purchase_option__totaled_cost_dmg_armr[2])
             if player_variables in brute_forced_options:
-                brute_forced_options[player_variables] = max(brute_forced_options[player_variables] , option_combined[0])
+                brute_forced_options[player_variables] = max(brute_forced_options[player_variables] , single_purchase_option__totaled_cost_dmg_armr[0])
             else:
-                brute_forced_options[player_variables] = option_combined[0]
+                brute_forced_options[player_variables] = single_purchase_option__totaled_cost_dmg_armr[0]
 
-print(f'The answer is {max(brute_forced_options.values())}')
+print(f'The answer is {max(brute_forced_options.values())}\n')
 
