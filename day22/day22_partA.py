a88d4a41bfd1516ffbab3628766865fb1982988a
Lewis Staples <lewis.staples@gmail.com>
2022-08-26 23:46:47 -0400

    Day 22 Rethinking program.  Now, player and boss have their own different classes.  Starting design with a complete universe of games, covering all choices

--- day22/day22_partA.py
+++ day22/day22_partA.py
@@ -7,9 +7,9 @@ from enum import Enum
 class LogicError(Exception):
     pass
 
-class PlayerID(Enum):
-    THE_PLAYER = 0,
-    THE_BOSS = 1
+# class PlayerID(Enum):
+#     THE_PLAYER = 0,
+#     THE_BOSS = 1
 
 class SpellID(Enum):
     MAGIC_MISSILE = 0,
@@ -18,121 +18,54 @@ class SpellID(Enum):
     POISON = 3,
     RECHARGE = 4
 
-@dataclass
+
 class Player:
-    # current state
+    def __init__(self, hit_points, mana):
+        # current state
+        self._hit_points = hit_points
+        self._damage = 0
+        self._armor = 0
+        self._mana = mana
+
+        # timers
+        self._shield_remaining = 0
+        self._recharge_remaining = 0
+
+@dataclass
+class Boss:
     _hit_points: int
     _damage: int
-    _armor: int
-    _mana: int
-    _id: PlayerID
-    _shield_remaining: int
-    _poision_remaining: int
-    _recharge_remaining: int
 
 class Game:
-    def __init__(self):
-        self._players = []
+    def __init__(self, player_hp, player_mana, boss_input_filename, games_list, show_rounds = False):
+        self._player = Player(player_hp, player_mana)
+        print(f'\nUsing boss paramater input file: {boss_input_filename}\n')
+        with open(boss_input_filename) as f:
+            # Pull in each line from the input file
+            for in_string in f:
+                in_string = in_string.rstrip()
+                varname, value = in_string.split(': ')
+                if varname == 'Hit Points':
+                    boss_hp = int(value)
+                elif varname == 'Damage':
+                    boss_damage = int(value)
+                # print(in_string)
+        self._boss = Boss(boss_hp, boss_damage)
         self._round_number = 0
+        self._show_rounds = show_rounds
+        self._games_list = games_list
 
-        # self._player_spells = None
+    def play_round(self):
+        dummy = 123
 
-    def add_player(self, player_to_add):
-        if self._round_number > 0:
-            raise LogicError("You can't add players while a game is playing")
-        self._players.append(player_to_add)
-    
-    def display_status(self):
-        print(f'Round # {self._round_number} -- {self._players[self._round_number%2]._id} turn')
-        for player in self._players:
-            # print(f'- {player._id} has {player._hit_points} hit points, {player._armor} armor, {player._mana} mana')
-            print(player)
-
-    # This returns None if there is no winner.  If someone has won, their name will be returned.
-    def play_round(self, spell=None):
-        index_attacker = self._round_number % 2
-        index_defender = (index_attacker + 1) % 2
-
-        damage = self._players[index_attacker]._damage
-        armor = self._players[index_defender]._armor
-
-        if self._players[index_attacker] == PlayerID.THE_PLAYER:
-            # Cast a spell
-            if spell is None:
-                # Determine all available spells (spells that are not yet active, have enough mana)
-                # Then (perhaps) create new thread with its own unique object
-                dummy = 123
-                raise LogicError('Functionality not yet implemented !!!') # reminder to come back to this
-            if spell is SpellID.MAGIC_MISSILE:
-                self._players[index_attacker]._mana -= 53
-                damage += 4
-            if spell is SpellID.DRAIN:
-                self._players[index_attacker]._mana -= 73
-                damage += 2
-                self._players[index_attacker]._hit_points += 2
-            if spell is SpellID.SHIELD:
-                self._players[index_attacker]._mana -= 113
-                self._players[index_attacker]._shield_remaining = 6
-            if spell is SpellID.POISON:
-                self._players[index_attacker]._mana -= 173
-                self._players[index_attacker]._poison_remaining = 6
-            if spell is SpellID.RECHARGE:
-                self._players[index_attacker]._mana -= 229
-                self._players[index_attacker]._recharge_remaining = 5
-
-            if self._players[index_attacker]._shield_remaining > 0:
-                armor += 7
-                self._players[index_attacker]._shield_remaining -= 1
-            if self._players[index_attacker]._poison_remaining > 0:
-                damage += 3
-                self._players[index_attacker]._poison_remaining -= 1
-            if self._players[index_attacker]._recharge_remaining > 0:
-                self.player[index_attacker]._mana += 101
-                self._players[index_attacker]._recharge_remaining -= 1
-
-        self._players[index_defender]._hit_points -= max(1, damage - armor)
-
-        self._round_number += 1
-        # If the attacker has won, return the attacker's name (who is the winner)
-        if self._players[index_defender]._hit_points <= 0:
-            return self._players[index_attacker]._id
-        
-        # Update remaining variables
-
-        # If there isn't a winner yet, return None
-        return None
-
-    def run_game(self, show_rounds = True, player_spells = {}):
-        if len(self._players) != 2:
-            raise LogicError('You can only play with exactly two players')
-
+    def run_game(self):
         while True:
-            # Displaying final status
-            if show_rounds:
-                self.display_status()
-                print()
-            if self._round_number in player_spells:
-                the_winner = self.play_round(player_spells[self._round_number])
-            else:
-                the_winner = self.play_round()
-            if the_winner is not None:
-                break
-        # Displaying final status
-        if show_rounds:
-            self.display_status()
-            print()
-        return the_winner
+            self.play_round()
 
-# Code created to test the above code
-# by reproducing the given sample data 
-print()
-the_game = Game()
-the_game.add_player( Player(10, 0, 0, 250, PlayerID.THE_PLAYER, 0, 0, 0) )
-the_game.add_player( Player(13, 8, 0, 0, PlayerID.THE_BOSS, 0, 0, 0) )
-the_game.run_game(True, {0 : SpellID.POISON, 2 : SpellID.MAGIC_MISSILE})
+            break # TO BE REPLACED LATER (for now it's here to prevent infinite loops)
 
-the_game = Game()
-the_game.add_player( Player(10, 0, 0, 250, PlayerID.THE_PLAYER, 0, 0, 0) )
-the_game.add_player( Player(14, 8, 0, 0, PlayerID.THE_BOSS, 0, 0, 0) )
-the_game.run_game(True, {0 : SpellID.RECHARGE, 2: SpellID.SHIELD, 4: SpellID.DRAIN, 6: SpellID.POISON, 8: SpellID.MAGIC_MISSILE})
+# First sample given:
+games_s0 = []
+games_s0.append(Game(10, 250, 'input_sample0.txt', games_s0, True))
+games_s0[0].run_game()
 
