0ec4614b0bb4cdfd8efdb8c4058cb679ae00f902
Lewis Staples <lewis.staples@gmail.com>
2022-08-27 22:29:43 -0400

    Day 22 More changes ... now class Boss and class Wizard will both inherit from class Player

--- day22/day22_partA.py
+++ day22/day22_partA.py
@@ -7,10 +7,6 @@ from enum import Enum
 class LogicError(Exception):
     pass
 
-# class PlayerID(Enum):
-#     THE_PLAYER = 0,
-#     THE_BOSS = 1
-
 class SpellID(Enum):
     MAGIC_MISSILE = 0,
     DRAIN = 1,
@@ -18,8 +14,13 @@ class SpellID(Enum):
     POISON = 3,
     RECHARGE = 4
 
-
 class Player:
+    pass
+
+    # _hit_points: int
+    # _damage: int
+
+class Wizard(Player):
     def __init__(self, hit_points, mana):
         # current state
         self._hit_points = hit_points
@@ -31,14 +32,15 @@ class Player:
         self._shield_remaining = 0
         self._recharge_remaining = 0
 
-@dataclass
-class Boss:
-    _hit_points: int
-    _damage: int
+# @dataclass
+class Boss(Player):
+    def __init__(self, hit_points, damage):
+        self._hit_points = hit_points
+        self._damage = damage
 
 class Game:
-    def __init__(self, player_hp, player_mana, boss_input_filename, games_list, show_rounds = False):
-        self._player = Player(player_hp, player_mana)
+    def __init__(self, wizard_hp, wizard_mana, boss_input_filename, games_list, rounds_to_show = []):
+        self._wizard = Wizard(wizard_hp, wizard_mana)
         print(f'\nUsing boss paramater input file: {boss_input_filename}\n')
         with open(boss_input_filename) as f:
             # Pull in each line from the input file
@@ -52,20 +54,22 @@ class Game:
                 # print(in_string)
         self._boss = Boss(boss_hp, boss_damage)
         self._round_number = 0
-        self._show_rounds = show_rounds
         self._games_list = games_list
+        self._rounds_to_show = rounds_to_show
 
-    def play_round(self):
-        dummy = 123
+    def show_status(self):
+        if len(self._rounds_to_show) == 0:
+            return # not displaying this one
+        print(f'-- turn --')
 
     def run_game(self):
         while True:
-            self.play_round()
-
+            # self.wizard_round()
+            # self.boss_round()
             break # TO BE REPLACED LATER (for now it's here to prevent infinite loops)
 
 # First sample given:
 games_s0 = []
-games_s0.append(Game(10, 250, 'input_sample0.txt', games_s0, True))
+games_s0.append(Game(10, 250, 'input_sample0.txt', games_s0, [SpellID.POISON, SpellID.MAGIC_MISSILE]))
 games_s0[0].run_game()
 
