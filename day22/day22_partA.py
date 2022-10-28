7b6592f7eab6431a80b0c4da414b91ae6a53c95a
Lewis Staples <lewis.staples@gmail.com>
2022-10-27 22:14:35 -0400

    Day 22 Progress: removed lots of code not being used (such as class Player), boss turns are now visible.  More is needed, though

--- day22/day22_partA.py
+++ day22/day22_partA.py
@@ -1,30 +1,10 @@
 # adventOfCode 2015 day 22
 # https://adventofcode.com/2015/day/22
 
-from dataclasses import dataclass  
-from enum import Enum
-from abc import ABC, abstractmethod
+
 import copy
-from webbrowser import get
 
-class LogicError(Exception):
-    pass
-
-# class SpellID(Enum):
-#     MAGIC_MISSILE = 0,
-#     DRAIN = 1,
-#     SHIELD = 2,
-#     POISON = 3,
-#     RECHARGE = 4
-
-class Player:
-    def __init__(self, hit_points, damage):
-        self.hit_points = hit_points
-        self.damage = damage
-
-
-
-class Wizard(Player):
+class Wizard(): # Player):
     # static variable
     spell_list = [
         {'spell_name': 'magic_missile', 'mana_cost': 53},
@@ -50,26 +30,21 @@ class Wizard(Player):
         self.recharge_remaining = 0
 
     def cast_magic_missile(self):
-        # self.mana -= 53
         self.magic_missile_remaining = 1
         self.damage += 4
     
     def cast_drain(self):
-        # self.mana -= 73
         self.damage += 2
         self.hit_points += 2
 
     def cast_shield(self):
-        # self.mana -= 113
         self.shield_remaining = 7
         self.armor += 7
 
     def cast_poison(self):
-        # self.mana -= 173
         self.poison_remaining = 7
 
     def cast_recharge(self):
-        # self.mana -= 229
         self.recharge_remaining = 5
 
 
@@ -77,9 +52,10 @@ class Wizard(Player):
     def get_status(self):
         return f'{self.hit_points} hit points, {self.armor} armor, {self.mana} mana'
 
-class Boss(Player):
+class Boss(): # Player):
     def __init__(self, hit_points, damage):
-        super().__init__(hit_points, damage)
+        self.hit_points = hit_points
+        self.damage = damage
 
     def get_status(self):
         return f'{self.hit_points} hit points'
@@ -119,70 +95,47 @@ class Game:
             ret_val += f'-- Boss turn --\n'
         ret_val += f'-Player has ' + self.wizard.get_status() + '\n'
         ret_val += f'-Boss has ' + self.boss.get_status() + '\n'
-
         return ret_val
 
-        # print(f'-Player has ', end='')
-        # print(self.wizard.get_status())
-        # print(f'-Boss has ', end='')
-        # print(self.boss.get_status())
 
     def round_start(self):  
-        # if len(self.rounds_to_show) > 0:
-        #     # if self.rounds_to_show[self.round_number]
-        #     to_print = True
-        # else:
-        #     to_print = False
-        # if to_print:
         ret_val = self.print_status()
         # Process Wizard's timers
         if self.wizard.magic_missile_remaining > 0:
             self.wizard.magic_missile_remaining -= 1
             if self.wizard.magic_missile_remaining == 0:
                 self.wizard.damage -= 4
-            # if to_print:
-            # print(f"Magic Missile was used.")
-            ret_val += "Magic Missile was used.\n"
+            ret_val += "Magic Missile deals 4 damage.\n"
         if self.wizard.drain_remaining > 0:
             self.wizard.drain_remaining -= 1
             if self.wizard.drain_remaining == 0:
                 self.wizard.damage -= 2
-            # if to_print:
-            #     print(f"Drain was used.")
-            ret_val += "Drain was used.\n"
+            ret_val += "Drain deals out 2 damage and heals wizard's 2 hit points.\n"
         if self.wizard.shield_remaining > 0:
             self.wizard.shield_remaining -= 1
             if self.wizard.shield_remaining == 0:
                 self.wizard.armor -= 7
-            # if to_print:
-            #     print(f"Shield's timer is now {self.wizard.shield_remaining}.")
             ret_val += f"Shield's timer is now {self.wizard.shield_remaining}.\n"
         if self.wizard.poison_remaining > 0:
             self.wizard.poison_remaining -= 1
             self.boss.hit_points -= 3
-            # if to_print:
-            #     print(f'Poison deals 3 damage; its timer is now {self.wizard.poison_remaining}.')
             ret_val += f'Poison deals 3 damage; its timer is now {self.wizard.poison_remaining}.'
         if self.wizard.recharge_remaining> 0:
             self.wizard.mana += 101
-            # if to_print:
-                # print(f'Recharge provides 101 mana; its timer is now {self.wizard.recharge_remaining}.')
             ret_val += f'Recharge provides 101 mana; its timer is now {self.wizard.recharge_remaining}.'
             self.wizard.recharge_remaining -= 1
         
 
-        # Calculate hit point loss of the party under attack
-        # Note for the wizard, this implies each of the scenarios
         if self.round_number % 2 == 0:
-            self.boss.hit_points -= max(1, self.wizard.damage)
             if self.boss.hit_points <= 0:
                 Game.min_mana_win = min(Game.min_mana_win, self.wizard.mana_spent)
-                return 'ROUND_ENDED'
+                ret_val += 'END'
 
         else:
+            # Boss attacking ... calculated using Day 21's logic
             self.wizard.hit_points -= max(1, self.boss.damage - self.wizard.armor)
             if self.wizard.hit_points <= 0:
-                return 'ROUND_ENDED'
+                ret_val += 'END'
 
         return ret_val
 # End of class Game
@@ -193,9 +146,12 @@ def play_game(game_list):
     while len(game_list) > 0:
         this_game = game_list.pop()
         ret_val = this_game.round_start()
-        if ret_val == 'ROUND_ENDED':
+        if ret_val[-3:] == 'END':
+            if this_game.rounds_to_show != []:
+                print(ret_val)
             continue
         if this_game.round_number % 2 == 0:
+            # Command for wizard's/player's turn
             for i in range(5):
                 spell_mana_cost = Wizard.spell_list[i]['mana_cost']
                 if spell_mana_cost > this_game.wizard.mana:
@@ -210,29 +166,33 @@ def play_game(game_list):
                 new_game.wizard.mana -= spell_mana_cost
                 new_game.wizard.mana_spent += spell_mana_cost
                 if new_game.rounds_to_show != []:
-                    if Wizard.spell_list[i]['spell_name'] != new_game.rounds_to_show.pop(0):
+                    popped_spell = new_game.rounds_to_show.pop(0)
+                    if Wizard.spell_list[i]['spell_name'] != popped_spell:
                         new_game.rounds_to_show = []
                     else:
                         print(ret_val)
                         print('Player casts ' + Wizard.spell_list[i]['spell_name'])
+                        if new_game.rounds_to_show == []:
+                            # This is to show the boss found following the last displayed wizard round
+                            # (if there is one)
+                            new_game.rounds_to_show = ['display_boss_round']
                 getattr(new_game.wizard, 'cast_' + Wizard.spell_list[i]['spell_name'])()
                 if new_game.wizard.hit_points < 0:
                     # The wizard ran out of h.p. ... doesn't count to objective
                     continue
                 # update round number
                 new_game.round_number += 1
-
                 # put back on the list
                 game_list.insert(0, new_game)
-                pass
-           
+                dummy = 123
+        
+        else:
+            # Commands for boss' turn
+            if this_game.rounds_to_show != []:
+                print(ret_val)
+            this_game.round_number += 1
+            game_list.insert(0, this_game)
             dummy = 123
-            
-        # Commands for boss' turn
-        if new_game.rounds_to_show != []:
-            print(ret_val)
-        this_game.round_number += 1
-        game_list.insert(0, this_game)
 
     print(f'\nThe minimum mana that can be spent on a win is {Game.min_mana_win}\n')
 
