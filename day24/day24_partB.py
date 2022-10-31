7e1ec1309a36aaf776428b4d888b3b539bbd8dbe
Lewis Staples <lewis.staples@gmail.com>
2022-10-31 11:49:18 -0400

    Day 24 Complete:  Parts A and B working

--- day24/day24_partB.py
+++ day24/day24_partB.py
@@ -6,19 +6,23 @@ import itertools
 
 
 # Determine if what's left over after group1 is defined
-# can be split into groups two and three,
-# where group three's sum is the targetted sum_third
-#
-# Since it is assumed that it was previously shown that group one sums to sum_third,
-# This means that groups 1, 2, and 3 are all equal, thus the group 1 choice is valid.
-def g1valid(g1combo, pkg_wts, sum_third):
-    g2and3combo = pkg_wts.difference(set(g1combo))
+# can be split into equal-summed groups 2, 3, and 4,
+def g1valid(g1combo, pkg_wts, sum_four):
+    g234combo = pkg_wts.difference(set(g1combo))
+    
+    # Consider all valid group two's
+    for i in range(1, len(g234combo)//3 + 1):
+        g2combo_iter = itertools.combinations(g234combo, i)
+        for g2combo in g2combo_iter:
+            if sum(g2combo) == sum_four:
+                g34combo = g234combo.difference(g2combo)
 
-    for i in range(1, len(g2and3combo)//2 + 1):
-        g3combo_iter = itertools.combinations(g2and3combo, i)
-        for g3combo in g3combo_iter:
-            if sum(g3combo) == sum_third:
-                return True
+                # Consider all valid group three's
+                for j in range(1, len(g34combo)//2 + 1):
+                    g3combo_iter = itertools.combinations(g34combo, j)
+                    for g3combo in g3combo_iter:
+                        if sum(g3combo) == sum_four:
+                            return True
     return False
 
 # Calculate quantum entanglement for this combination
@@ -42,21 +46,21 @@ with open(input_filename) as f:
 
 len_pkg = len(pkg_wts)
 
-if sum(pkg_wts) % 3 != 0:
-    raise ValueError("The given list's sum divided by three isn't an integer!")
+if sum(pkg_wts) % 4 != 0:
+    raise ValueError("The given list's sum divided by four isn't an integer!")
 
-sum_third = int(sum(pkg_wts) / 3)
+sum_four = int(sum(pkg_wts) / 4)
 
 for len_g1 in range(1, len_pkg):
     # Consider all combinations of this length
     all_combos_of_this_length = itertools.combinations(pkg_wts, len_g1)
     for g1combo in all_combos_of_this_length:
-        # Disregard any combination choices for group1 which don't sum up to sum_third
-        if sum(g1combo) != sum_third:
+        # Disregard any combination choices for group1 which don't sum up to sum_four
+        if sum(g1combo) != sum_four:
             continue
         # Perform other checks to see if this group1 choice is valid
         # (Can the remainder be split into equal-summed groups 2 and 3?)
-        if g1valid(g1combo, pkg_wts, sum_third):
+        if g1valid(g1combo, pkg_wts, sum_four):
             smallest_qu_ent = min(smallest_qu_ent, quantum_entanglement(g1combo))
     if smallest_qu_ent < float('inf'):
         break
